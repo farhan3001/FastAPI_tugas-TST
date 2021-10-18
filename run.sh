75107a4a363fc6831ba64253751e36da27d06122
288-Farhan <farhansimatupang@gmail.com>
2021-10-18 13:23:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 uvicorn main:app --reload
 pip install PyJWT==1.7.1 python-decouple==3.3
 pip install pydantic[email]
-heroku ps:scale web=1 --app tugas-fastapi-tst
+heroku ps:scale web=4 --app tugas-fastapi-tst
