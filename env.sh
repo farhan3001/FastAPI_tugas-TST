fe947e226665270213eb9154151cfc899094248e
288-Farhan <farhansimatupang@gmail.com>
2021-10-18 13:20:15 +0700

    commit

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 pipenv install
 pipenv shell
-pip install PyJWT==1.7.1 python-decouple==3.3
-pip install pydantic[email]
+# pip install PyJWT==1.7.1 python-decouple==3.3
+# pip install pydantic[email]
