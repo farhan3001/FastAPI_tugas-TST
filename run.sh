db577f6c9af88566345b146343d95c84ab802b8b
288-Farhan <farhansimatupang@gmail.com>
2021-10-19 14:10:44 +0700

    commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-uvicorn main:app --reload
+uvicorn --port 8000 --host 127.0.0.1 main:app --reload
 # pip install PyJWT==1.7.1 python-decouple==3.3
 # pip install pydantic[email]
 # heroku ps:scale web=4 --app tugas-fastapi-tst
