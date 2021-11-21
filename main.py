6ade11b42042fe25b0c15b56310a3b876c3da1e1
farhan3001 <74050546+farhan3001@users.noreply.github.com>
2021-11-21 14:32:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -116,8 +116,8 @@ async def user_LogIn (username: str, password: str):
         return signJWT(user),{"Login was successful"}
     return {"error": "Login was unsuccessful, your username or password might be incorrect"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000,reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=5000,reload=True)
     
 # @app.post('/menu', dependencies=[Depends(JWTBearer())], tags=["posts"])
 # async def post_menu(name:str):
